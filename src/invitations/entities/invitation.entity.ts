5ff59bcd2326f55b2b074acfd3e05023207b36b9
Juan Rogelio Franco Martinez <juanframart2011@gmail.com>
2024-02-17 22:45:55 -0600

    agregando entity

--- src/invitations/entities/invitation.entity.ts
+++ src/invitations/entities/invitation.entity.ts
@@ -1,1 +1,26 @@
-export class Invitation {}
+import { User } from "src/users/entities/user.entity";
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, JoinColumn, ManyToOne } from "typeorm";
+
+@Entity()
+export class Invitation {
+    @Column({ primary: true, generated: true })
+    id: number;
+    @Column({ nullable: false })
+    user_id: string;
+    @Column({ nullable: false })
+    name: string;
+    @Column({ type: 'timestamp', nullable: false })
+    entry_date: Date;
+    @Column({ type: 'timestamp', nullable: false })
+    expiration: Date;
+    @CreateDateColumn()
+    created_at: Date;
+    @DeleteDateColumn({ type: 'timestamp', nullable: true })
+    deleted_at: Date;
+    @CreateDateColumn()
+    updated_at: Date;
+
+    @ManyToOne(() => User, user => user.invitations)
+    @JoinColumn({ name: "user_id" })
+    user: User;
+}
